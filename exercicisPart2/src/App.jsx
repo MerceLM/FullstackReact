9af03ec62a43ca1d81a6cbc1d102aa7989ad7588
Merce Lozano <mlozano@dgpdocen.caib.es>
2023-06-23 12:24:53 +0200

    Part 2 - Exercici 2.9 - Afegit camp de cerca per filtrar el llistat per nom de la persona

--- exercicisPart2/src/App.jsx
+++ exercicisPart2/src/App.jsx
@@ -9,7 +9,12 @@ const App = () => {
     ])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
+    const [ filterName, setFilterName ] = useState('')
 
+
+    const handleChangeFilterName = (e) => {
+        setFilterName(e.target.value)
+    }
 
     const handleChangeName = (e) => {
         setNewName(e.target.value)
@@ -38,6 +43,8 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>Filter shown with <input type="text" onChange={handleChangeFilterName} value={filterName} /></div>
+            <h2>Add a new</h2>
             <form>
                 <div>Name: <input type="text" value={newName} onChange={handleChangeName} /></div>
                 <div>Number: <input type="text" value={newNumber} onChange={handleChangeNumber} /></div>
@@ -46,7 +53,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(p =>   <div key={p.name}>{p.name} {p.number}</div> )}
+            {persons.map(p =>  p.name.toUpperCase().startsWith(filterName.toUpperCase()) && <div key={p.name}>{p.name} {p.number}</div> )}
         </div>
     )
 }
