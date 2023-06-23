3ba000464ad9fda42a53b8789217328667d4b47a
Merce Lozano <mlozano@dgpdocen.caib.es>
2023-06-23 11:58:55 +0200

    Part 2 - Exercici 2.6

---
+++ exercicisPart2/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [ persons, setPersons ] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [ newName, setNewName ] = useState('')
+
+
+    const handleChangeName = (e) => {
+        setNewName(e.target.value)
+    }
+    const addNewPerson = e => {
+        e.preventDefault()
+        const newNameObj = {
+            name: newName
+        }
+        setPersons(persons.concat(newNameObj))
+        setNewName('')
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    Name: <input type="text" value={newName} onChange={handleChangeName} />
+                </div>
+                <div>
+                    <button type="submit" onClick={addNewPerson}>add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(p =>   <div key={p.name}>{p.name}</div> )}
+        </div>
+    )
+}
+
+export default App
