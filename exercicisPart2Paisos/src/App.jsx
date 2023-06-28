ebb854b93b9605518b61bd3973811a8ffc233aec
Merce Lozano <mlozano@dgpdocen.caib.es>
2023-06-28 10:11:41 +0200

    Part 2 - Exercici 2.12. Recuperació de països d'una API externa. Amb filtre. Mostra la informació d'un país si el filtre es correspon amb només 1, un llistat dels països si el filtre es correspon amb 2 o fins a 10 i un text per millorar la cerca si el filtre torna més de 10 països.

--- exercicisPart2Paisos/src/App.jsx
+++ exercicisPart2Paisos/src/App.jsx
@@ -1,8 +1,10 @@
-import {useEffect, useState} from 'react'
+import {Fragment, useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
     const [countries, setCountries] = useState([])
+    const [filterResult, setFilterResult] = useState([])
+    const [filter, setFilter] = useState('')
 
     useEffect(() => {
         console.log('effect a App')
@@ -23,14 +25,49 @@ function App() {
     }, [])
     console.log('rendered ', countries.length, ' countries')
 
+    function handleFilterChange(e){
+        let valor = e.target.value
+        setFilter(valor)
+        if (valor !== ''){
+            let paisosTrobats = countries.filter(c => c.name.common.toUpperCase().startsWith(valor.toUpperCase()))
+            setFilterResult(paisosTrobats)
+        }else{
+            setFilterResult([])
+        }
+
+
+    }
+
+    function renderSwitch (fr){
+        if ( fr.length > 10) {
+            return <div>Too many matches, specify another filter</div>
+
+        }else if (fr.length === 1) {
+            let pais = fr[0]
+            let idiomes = Object.values(pais.languages)
+            return (
+                <Fragment>
+                    <h3>{pais.name.common}</h3>
+                    <div>Capital: {pais.capital.map(p => p)}</div>
+                    <p>Population: {pais.population}</p>
+                    <h3>Languages</h3>
+                    <ul>{idiomes.map(pa => <li key={pa}>{pa}</li>)}</ul>
+                    <img width="300px"  key="bandera" src={pais.flags.svg} />
+                </Fragment>
+            )
+
+        }else {
+            return fr.map(c =>  <div key={c.name.common}>{c.name.common}</div> )
+        }
+    }
 
   return (
     <>
-        {countries.map(c =>
-            <div>{c.name.common}</div>
-
-            )}
-
+        <div>
+            <h1>Exercici 2.12</h1>
+            <h2>Search countries <input type="text" value={filter} onChange={handleFilterChange} /></h2>
+        </div>
+        <div>{renderSwitch(filterResult)}</div>
     </>
   )
 }
