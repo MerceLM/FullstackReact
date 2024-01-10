54a9ec17fcaec0a78f2a1a7b7b0b045dfd1de97d
Merce Lozano <mlozano@dgpdocen.caib.es>
2024-01-10 09:20:13 +0100

    Part 3 acabada. Apartat c no fet.

--- part3/.eslintrc.js
+++ part3/.eslintrc.js
@@ -1,13 +1,46 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true,
-
-        "jest": true,
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
     },
-    "extends": "eslint:recommended",
-    "rules": {
-        // ...
+    'extends': 'eslint:recommended',
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
     },
-};
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-trailing-spaces' : 'error',
+        'object-curly-spacing' : [
+            'error', 'always'
+        ]
+    }
+}
--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {request, response} = require("express");
+//const {request, response} = require("express")
 const app = express()
 
 app.use(express.json()) //Per poder accedir a les dades enviades en el body d'una peticio POST
@@ -8,20 +8,20 @@ app.use(express.json()) //Per poder accedir a les dades enviades en el body d'un
 let notes = [
     {
         id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
+        content: 'HTML is easy',
+        date: '2019-05-30T17:30:31.098Z',
         important: true
     },
     {
         id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
+        content: 'Browser can execute only Javascript',
+        date: '2019-05-30T18:39:34.091Z',
         important: false
     },
     {
         id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        date: '2019-05-30T19:20:14.298Z',
         important: true
     }
 ]
